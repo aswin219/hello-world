26b3ed6fb9b5a12759515f1664e0f1dc3c2e4d2e
aswin219 <74107290+aswin219@users.noreply.github.com>
2025-07-11 11:40:13 +0530

    Create test.sh

---
+++ sg/test.sh
@@ -0,0 +1,11 @@
+ip=$(curl -s https://ipinfo.io/ip | tr -d '[:space:]')
+echo "Runner IP is $ip"
+sleep 10
+ip=$(curl -s https://ipinfo.io/ip | tr -d '[:space:]')
+echo "Runner IP is $ip"
+sleep 10
+ip=$(curl -s https://ipinfo.io/ip | tr -d '[:space:]')
+echo "Runner IP is $ip"
+sleep10
+ip=$(curl -s https://ipinfo.io/ip | tr -d '[:space:]')
+echo "Runner IP is $ip"
